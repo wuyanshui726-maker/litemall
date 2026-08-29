2f17c1422df3f50e82870043338b5a115cc8327d
yanshui <wuyanshui726@gmail.com>
2026-08-29 18:02:27 +0800

    fix: 分类页终于修复!改对了catalog文件+ucenter对齐
    【关键修复】
    - pages/catalog/catalog.js: 彻底重写!不再调用旧 CatalogList API
      - 硬编码2个分类(食物2000000/饮品2000001)
      - 改用 GoodsList API 拉商品
      - wx.getSystemInfo 动态计算 scrollHeight
    - pages/catalog/catalog.wxml: 重写为左栏tab+右栏商品网格(参考用户图)
    - pages/catalog/catalog.wxss: 重写flex布局样式
    - pages/category/category*: 之前改错的文件,现在终于改对了catalog!
    
    【ucenter对齐】
    - 删除意见反馈选项
    - 所有菜单项图标统一用 image class='icon' 48rpx
    - 删掉混用的 emoji 📦 和 icon-text 元素
    - 所有菜单项高度统一100rpx,白色色块,左对齐
    - divider 缩进对齐到 icon 之后
    
    API 验证: GoodsList?categoryId=2000000 返回 9 款商品 ✅
    后端正常运行 ✅

--- litemall-wx/pages/catalog/catalog.js
+++ litemall-wx/pages/catalog/catalog.js
@@ -1,77 +1,64 @@
 var util = require('../../utils/util.js');
 var api = require('../../config/api.js');
 
+// 批发改造：硬编码2个分类，不再依赖 CatalogList API
+var NAV_LIST = [
+  { id: 2000000, name: '食物' },
+  { id: 2000001, name: '饮品' }
+];
+
 Page({
   data: {
-    categoryList: [],
-    currentCategory: {},
-    currentSubCategoryList: {},
-    scrollLeft: 0,
-    scrollTop: 0,
+    navList: NAV_LIST,
+    id: 2000000,
+    goodsList: [],
     goodsCount: 0,
-    scrollHeight: 0
+    scrollHeight: 600
   },
-  onLoad: function(options) {
-    this.getCatalog();
-  },
-  onPullDownRefresh() {
-    wx.showNavigationBarLoading() //在标题栏中显示加载
-    this.getCatalog();
-    wx.hideNavigationBarLoading() //完成停止加载
-    wx.stopPullDownRefresh() //停止下拉刷新
-  },
-  getCatalog: function() {
-    //CatalogList
-    let that = this;
-    wx.showLoading({
-      title: '加载中...',
-    });
-    util.request(api.CatalogList).then(function(res) {
-      that.setData({
-        categoryList: res.data.categoryList,
-        currentCategory: res.data.currentCategory,
-        currentSubCategoryList: res.data.currentSubCategory
-      });
-      wx.hideLoading();
-    });
-    util.request(api.GoodsCount).then(function(res) {
-      that.setData({
-        goodsCount: res.data
-      });
-    });
 
-  },
-  getCurrentCategory: function(id) {
-    let that = this;
-    util.request(api.CatalogCurrent, {
-        id: id
-      })
-      .then(function(res) {
-        that.setData({
-          currentCategory: res.data.currentCategory,
-          currentSubCategoryList: res.data.currentSubCategory
-        });
-      });
-  },
-  onReady: function() {
-    // 页面渲染完成
-  },
-  onShow: function() {
-    // 页面显示
-  },
-  onHide: function() {
-    // 页面隐藏
-  },
-  onUnload: function() {
-    // 页面关闭
-  },
-  switchCate: function(event) {
+  onLoad: function() {
     var that = this;
-    var currentTarget = event.currentTarget;
-    if (this.data.currentCategory.id == event.currentTarget.dataset.id) {
-      return false;
-    }
+    wx.getSystemInfo({
+      success: function(res) {
+        // 减去顶部搜索栏高度 88rpx ≈ 44px，减去 tabBar 高度
+        that.setData({
+          scrollHeight: res.windowHeight - 44 - 50
+        });
+      }
+    });
+    that.loadData();
+  },
 
-    this.getCurrentCategory(event.currentTarget.dataset.id);
+  onShow: function() {
+    this.setData({ goodsList: [] });
+    this.loadGoods();
+  },
+
+  loadData: function() {
+    var that = this;
+    util.request(api.GoodsCount).then(function(res) {
+      that.setData({ goodsCount: res.data });
+    });
+    that.loadGoods();
+  },
+
+  loadGoods: function() {
+    var that = this;
+    util.request(api.GoodsList, {
+      categoryId: that.data.id,
+      page: 1,
+      limit: 100
+    }).then(function(res) {
+      that.setData({ goodsList: res.data.list || [] });
+    }).catch(function(err) {
+      console.error('加载商品失败', err);
+    });
+  },
+
+  switchCate: function(event) {
+    var id = event.currentTarget.dataset.id;
+    if (this.data.id == id) return;
+    this.setData({ id: id, goodsList: [] });
+    this.loadGoods();
   }
-})
+});
